f578a255b039aba16a100591a3063152e881f474
Malmsteg <73699667+Malmsteg@users.noreply.github.com>
2021-12-05 13:41:33 +0100

    first

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System.IO;
+using System.Collections.Generic;
+using System;
+
+namespace AdventOfCode_2021_Day5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string currentDirectory = AppDomain.CurrentDomain.BaseDirectory;
+            string file = Path.Combine(currentDirectory, @"..\..\..\test.txt");
+            string path = Path.GetFullPath(file);
+            string[] text = File.ReadAllText(path).Split("\n");
+            const int LENGTH = 1000;
+
+            string currentLine = text[0];   // split current line into 4 coordinates, 
+                                            //  x1,x2,y1,y2 ONLY horizontal or vertical
+
+
+
+
+
+
+
+        }
+    }
+}
